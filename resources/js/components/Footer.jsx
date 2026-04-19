02be0cdc4e5b91aee18186e5f2c015fd19887a9c
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-04-19 16:05:05 +0200

    footer

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -3,22 +3,26 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="mt-6 text-white pt-4 text-center text-sm text-slate-600">
-            A kiadvány a „Táncház-Népzene 2026” hanglemezpályázat nyerteseinek hangfelvételeit tartalmazza.
-This album presents winners of the „Dance House – Folk Music 2026” record competition.
+            <p>
+            <b>A kiadvány a „Táncház-Népzene 2026” hanglemezpályázat nyerteseinek hangfelvételeit tartalmazza.
+                This album presents winners of the „Dance House – Folk Music 2026” record competition.</b>
 
-Köszönet a zsűri tagjainak / Special thanks to members of the jury:
-Agócs Gergely, Árendás Péter, Berta Alexandra, Vavrinecz András.
-
-A hangfelvételek a Barázda Stúdióban készültek, 2026. január–február havában.
-Recorded at the Barázda Studio, in January and February, 2026.
-
-Hangmérnök / Recording engineer: Dióssy Ákos
-Zenei rendező / Production director-editor: Árendás Péter
-Fotó / Photo: Dusa Gábor
-(Horsa István és tanítványa, Jocha Áron az Ördöngös Népzenetanodából, 2025 / István Horsa and his student Áron Jocha from the Ördöngös Folk Music School, 2025)
-
-Grafika / Graphics: Arany Imre | Layout Factory
-Kiadó / Produced and released by: Hagyományok Háza / Hungarian Heritage House
+                </p>
+            <p>
+                Köszönet a zsűri tagjainak / <i>Special thanks to members of the jury</i>:
+                Agócs Gergely, Árendás Péter, Berta Alexandra, Vavrinecz András.
+            </p>
+            <p>
+                A hangfelvételek a Barázda Stúdióban készültek, 2026. január–február havában. / <i>Recorded at the Barázda Studio, in January and February, 2026.</i>
+            </p>
+            <p>
+                Hangmérnök / <i>Recording engineer</i>: <b>Dióssy Ákos</b> |
+                Zenei rendező / <i>Production director-editor</i>: <b>Árendás Péter</b> |
+                Fotó / <i>Photo</i>: <b>Dusa Gábor</b>
+            </p>
+            <p>(Horsa István és tanítványa, Jocha Áron az Ördöngös Népzenetanodából, 2025 / <i>István Horsa and his student Áron Jocha from the Ördöngös Folk Music School, 2025</i>)</p>
+            <p>Grafika / <i>Graphics</i>: <b>Arany Imre | Layout Factory</b></p>
+            <p>Kiadó / <i>Produced and released by</i>: <b>Hagyományok Háza / Hungarian Heritage House</b></p>
         </footer>
     );
 }
