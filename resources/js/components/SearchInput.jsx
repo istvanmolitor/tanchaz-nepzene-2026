72272b191c15eb2ddab738dc8032139e21732c39
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-04-24 09:20:50 +0200

    search and title

--- resources/js/components/SearchInput.jsx
+++ resources/js/components/SearchInput.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function SearchInput({ value, onChange }) {
     return (
-        <div className="mb-5 flex items-center justify-end gap-2">
-            <label htmlFor="search" className="text-white md:text-slate-700 text-2xl font-bold">
+        <div className="mb-5 flex flex-col items-center justify-center gap-2 md:flex-row md:justify-end">
+            <label htmlFor="search" className="text-center text-2xl font-bold text-white md:text-slate-700">
                 KERESÉS
             </label>
             <input
